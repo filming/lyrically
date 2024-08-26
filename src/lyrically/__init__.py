38262f930eb8ec517730db99c8bf335bba2eba76
Filming <79931565+filming@users.noreply.github.com>
2024-08-26 13:11:57 -0400

    chore: Initital commit

---
+++ src/lyrically/__init__.py
@@ -0,0 +1,5 @@
+class Lyrically:
+    """A class which contains methods used to retrieve and store song data."""
+
+    def __init__(self) -> None:
+        pass
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from lyrically import Lyrically
+
+
+def main():
+    lyrically = Lyrically()
+
+
+if __name__ == "__main__":
+    main()
