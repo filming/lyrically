09e9078c322a49e042888287e0042f2cb88733af
Filming <79931565+filming@users.noreply.github.com>
2024-08-27 15:53:23 -0400

    style: Remove test code from main file

--- src/main.py
+++ src/main.py
@@ -4,9 +4,6 @@ from lyrically import Lyrically
 def main():
     lyrically = Lyrically()
 
-    artist_name = "Justin BIEber"
-    discography = lyrically.get_artist_discography(artist_name)
-
 
 if __name__ == "__main__":
     main()
