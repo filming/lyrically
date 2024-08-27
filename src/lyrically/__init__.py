5ccd003c805ccdc51f34f2bb12488e2a25bd2b28
Filming <79931565+filming@users.noreply.github.com>
2024-08-27 11:16:07 -0400

    feat: Get artist data in html format

--- src/lyrically/__init__.py
+++ src/lyrically/__init__.py
@@ -5,4 +5,15 @@ class Lyrically:
     """A class which contains methods used to retrieve and store song data."""
 
     def __init__(self) -> None:
+        self.BASE_URL = "https://www.azlyrics.com/"
         self.s = requests.Session()
+
+    def get_artist_data(self, artist_name: str):
+        """Retrieve links of an artist's full catalouge."""
+
+        artist_name = artist_name.lower().replace(" ", "")
+        artist_page_url = self.BASE_URL + f"{artist_name[0]}/{artist_name}.html"
+
+        r = self.s.get(artist_page_url)
+        print(r.status_code)
+        print(r.text)
--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from lyrically import Lyrically
 def main():
     lyrically = Lyrically()
 
+    artist_name = "Justin BIEber"
+    lyrically.get_artist_data(artist_name)
+
 
 if __name__ == "__main__":
     main()
